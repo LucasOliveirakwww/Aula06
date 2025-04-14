e16d1a6674040853f27da0265109437a0ad4c6f7
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-04-14 15:39:18 -0300

    Exercício 03 da aula 06

---
+++ Exercício 03 da aula 06.py
@@ -0,0 +1,9 @@
+#Fazer código para ler 2 valores, fazer divisão do 1º pelo 2º e caso o segundo for 0, solicitar novamente informando que só aceitamos valores diferentes de 0
+quant1 = int(input("Digite um valor: " ))
+quant2 = int(input("Digite um valor: "))
+while quant2 == 0:
+    quant2 = int(input("Digite um valor(0 = inválido): "))
+divis = quant1 / quant2
+print(divis)
+
+
